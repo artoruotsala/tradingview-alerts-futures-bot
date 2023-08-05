62c034f48d4e7f6cf171e7ff9614a43bd8beea01
Arto Ruotsala <arto.ruotsala@gmail.com>
2023-08-05 23:54:29 +0300

    add euro price to each trade (in logger)

--- src/services/trade.logger.ts
+++ src/services/trade.logger.ts
@@ -1,25 +1,46 @@
-import { Order } from 'ccxt'
+import { Exchange, Order } from 'ccxt'
 import fs from 'fs'
+import { TradingAccount } from './trading/trading.account'
+
+export interface OrderWithEURPrice extends Order {
+  priceEUR?: number
+}
 
 const filename = '/bot/orders.json'
 
-export function writeOrderToFile(order: Order) {
+export async function getTUSDEURRate() {
+  const exchange = TradingAccount.getInstance()
+
+  const ticker = await exchange.getTicker('TUSD/EUR')
+  return ticker.last
+}
+
+export async function writeOrderToFile(order: Order) {
+  const rate = await getTUSDEURRate()
+
+  // Convert the order price to EUR
+  const priceInEUR = order.price * rate
+
+  // Create a new object with the EUR price added
+  const orderWithEURPrice = {
+    ...order,
+    priceEUR: priceInEUR,
+  } as OrderWithEURPrice
+
   fs.readFile(filename, 'utf8', (err, data) => {
     if (err) {
       console.log(`Error reading file from disk: ${err}`)
     } else {
-      // Attempt to parse JSON string to JSON object
-      let orders
+      let orders: OrderWithEURPrice[]
       try {
         orders = JSON.parse(data)
-      } catch (parseErr) {
-        console.error(`Error parsing JSON string: ${parseErr}`)
-        // Set a default value (empty array) if parsing fails
-        orders = []
+      } catch (err) {
+        console.log(`Error parsing JSON string: ${err}`)
+        return
       }
 
       // add the new order to existing orders
-      orders.push(order)
+      orders.push(orderWithEURPrice)
 
       // write back to file
       fs.writeFile(filename, JSON.stringify(orders, null, 2), (err) => {
